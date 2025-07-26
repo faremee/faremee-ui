b2e564d79d47eb941992085f12df81065857cd1e
faremee <faremee11@gmail.com>
2025-07-26 14:37:30 -0400

    Update FlightCard.jsx

--- components/FlightCard.jsx
+++ components/FlightCard.jsx
@@ -1,16 +1,20 @@
-// components/FlightCard.jsx
 import Link from "next/link";
 import {
-  AirplaneIcon,
   CurrencyDollarIcon,
   ClockIcon,
   TicketIcon,
+  PaperAirplaneIcon // <-- Use PaperAirplaneIcon instead of AirplaneIcon
 } from "@heroicons/react/24/outline";
 
 export default function FlightCard({ flight }) {
   return (
     <Link href={`/detail/flights/${flight.id}`}>
       <a className="block bg-white dark:bg-gray-800 rounded-lg shadow hover:shadow-lg transition p-4 space-y-2">
+        {/* Example icon usage */}
+        <div className="flex items-center space-x-2">
+          <PaperAirplaneIcon className="h-6 w-6 text-blue-600" />
+          <span>{flight.airline}</span>
+        </div>
         {/* …rest of your JSX… */}
       </a>
     </Link>
